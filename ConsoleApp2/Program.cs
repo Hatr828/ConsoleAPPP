3b7fab3c229478a607b0bba00a8bded3cf52674d
Hatr727 <hatr626@gmail.com>
2024-07-01 15:43:40 -0500

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName= "notepad.exe";
+            process.Exited += Fineshed;
+            process.Start();
+            process.Close();
+        }
+
+        public static void Fineshed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process done");
+        }
+    }
+}
