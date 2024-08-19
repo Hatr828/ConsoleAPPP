ba2239be1d71cddbcc9f89d01d3bbd81516bbc6b
Hatr727 <hatr626@gmail.com>
2024-08-18 22:12:02 -0500

    0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1",8888);
+            NetworkStream stream = client.GetStream();
+
+            byte[] request = Encoding.ASCII.GetBytes("date");
+            stream.Write(request, 0, request.Length);
+
+            byte[] data = new byte[256];
+            StringBuilder response = new StringBuilder();
+            int bytes;
+
+            bytes = stream.Read(data,0,data.Length);
+            response.Append(Encoding.ASCII.GetString(data, 0 ,bytes));
+
+            client.Close();
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,31 +1,65 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        while (true)
-        {
-            string input = Console.ReadLine();
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8888;
+        TcpListener listener = new TcpListener(ipAddress, port);
 
-            try
+        try
+        {
+            listener.Start();
+
+            while (true)
             {
-                double result = Test(input);
-                Console.WriteLine(result);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"error");
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("Test");
+
+                byte[] buffer = new byte[256];
+                stream.Read(buffer,0,buffer.Length);
+                String request = Encoding.ASCII.GetString(buffer);
+
+                if (request.Equals("date",StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("date");
+
+                    string date = DateTime.Now.ToShortDateString();                                                     
+                    byte[] data = Encoding.ASCII.GetBytes(date);
+
+                    stream.Write(data, 0, data.Length);
+                }
+                else if (request.Equals("time", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("time");
+
+                    string time = DateTime.Now.ToShortTimeString();
+                    byte[] data = Encoding.ASCII.GetBytes(time);
+
+                    stream.Write(data, 0, data.Length);
+                }
+                else
+                {
+                    Console.WriteLine("test2");
+                }
+
             }
         }
+        catch(Exception ex)
+        {
+                  Console.WriteLine(ex.Message);                            
+        }
+        finally
+        {
+            listener.Stop();
+        }
 
-    }
-
-    static double Test(string expression)
-    {
-        var dataTable = new System.Data.DataTable();
-        var result = dataTable.Compute(expression, string.Empty);
-        return Convert.ToDouble(result);
     }
 }
